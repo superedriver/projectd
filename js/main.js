4940eb4dfdb094bac98b137c0b372bd3cd1d3c02
superedriver <superedriver@gmail.com>
2015-03-05 00:44:19 +0200

    new message if you click on user, reload page if you click on '÷ÈÏÄÑÝÉÅ'

--- js/main.js
+++ js/main.js
@@ -2,14 +2,28 @@ $(function(){
 
 function showUsers() {
 	var userList = $('<ul>').appendTo('.right-column');
-	
+
 	for (var i = 0; i < users.length; i++) {
 		var userInfo = $('<li>').appendTo(userList);
-		var userString = '<img alt="' + users[i].name + '" src="' + users[i].logo + '">' + users[i].name
+		var userString = '<img alt="' + users[i].name + '" src="' + users[i].logo + '">' + users[i].name;
 		userInfo.html(userString);
 	}
 }
 
+function createNewMessage() {
+	var stringCreateNewMessage = '<div id="new-message"><h3>Новое сообщение для:<a href="">' + $(this).text() + '</a></h3><form class="form-horizontal"><textarea class="form-control" rows="2" id="textArea" placeholder="Введите сообщение" ></textarea><a href="#" class="btn btn-default">Отправить</a></form></div>'
+	$('.wrapper').html(stringCreateNewMessage);
+}
+
 showUsers();
+
+$('.right-column ul li').click(createNewMessage);
+$('.header>.wrap>a').click(function() {
+	location.reload();
+	});
+
+
+
+
 
 });
