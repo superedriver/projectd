4c89c76f79a9b0dc920e68774a4473b33e4b87a5
superedriver <superedriver@gmail.com>
2015-03-13 01:33:43 +0200

    before template

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ function showUsers() {
 
   for (var i in users) {
     var userInfo = $('<li>').appendTo(userList);
-    var userString = '<img alt="' + users[i].name + '" src="' + users[i].logo + '">' + users[i].name;
+    var currentUser = users[i];
+    var userString = '<img alt="' + currentUser.name + '" src="' + currentUser.logo + '">' + currentUser.name;
+    // var userString = '<img alt="' + users[i].name + '" src="' + users[i].logo + '">' + users[i].name;
     userInfo.html(userString);
   }
 }
@@ -35,38 +37,27 @@ function  showDialogs() {
     }
 
     var months =['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
-    // index of this dialog in dialogs[]
-    var indexDialog = _.findIndex(dialogs, {'id': dialogId});
-    var thisDialog = dialogs[indexDialog];
-    // message creator's index in users[]  
-    var indexMessageSender = _.findIndex(users, {'id': dialogs[indexDialog].sender});
-    // message receiver's index in users[]    
-    var indexMessageReceiver = _.findIndex(users, {'id': dialogs[indexDialog].receiver});
-    // text of message index in messages[]      
-    var indexMessage = _.findIndex(messages, {'id': dialogs[indexDialog].text});
-    var thisMessage = messages[indexMessage];
-
+    // index of this message in messages[]
+    var indexDialog = _.findIndex(messages, {'id': dialogId});
+    var thisMessage = messages[indexDialog];
+    // message sender in users[]  
+    var messageSender = _.find(users, {'id': thisMessage.sender_id});
+    // message receiver in users[]    
+    var messageReceiver = _.find(users, {'id': thisMessage.receiver_id});
     // this user's index in users[]
-    var indexThisUser = _.findIndex(users, {'id': idThisUser});
-    
+    var thisUser = _.find(users, {'id': idThisUser});
     // recipient's index in users[]
-    var indexRecipient = indexMessageSender;
-    if (indexRecipient == indexThisUser) {indexRecipient = indexMessageReceiver};
-
-    var recipientName = users[indexRecipient].name;
-    var recipientLogo = users[indexRecipient].logo;
-    // users[0] - Maks
-    var senderName = users[indexMessageSender].name;
-    var senderLogo = users[indexMessageSender].logo;
+    var recipient = messageSender;
+    if (recipient == thisUser) {recipient = messageReceiver};
     // reduction messageTime to view - DD Month YYYY в HH:MM:SS
-    var messageTime = addNull(thisDialog.time.getDate()) + ' ' + months[thisDialog.time.getMonth()] + ' ' + thisDialog.time.getFullYear() + ' в ' + addNull(thisDialog.time.getHours()) + ':' + addNull(thisDialog.time.getMinutes()) + ':' + addNull(thisDialog.time.getSeconds());
+    var messageTime = addNull(thisMessage.created_at.getDate()) + ' ' + months[thisMessage.created_at.getMonth()] + ' ' + thisMessage.created_at.getFullYear() + ' в ' + addNull(thisMessage.created_at.getHours()) + ':' + addNull(thisMessage.created_at.getMinutes()) + ':' + addNull(thisMessage.created_at.getSeconds());
     // Cutting the long text
-    var messageText = getMessageText(thisMessage.text);
+    var messageText = getMessageText(thisMessage.text); 
     // Create and append the dialog
-    var dialog = '<div class="dialog"><div class="dialogs-photo"><img alt="' + recipientName + '" src="' + recipientLogo + '"></div><div class="dialogs-info"><div class="dialogs-info-name h3">' + recipientName + '</div><div class="dialogs-info-date text-muted">' + messageTime + '</div></div><div class="dialogs-msg-contents"><div class="dialogs-message-image"><img alt="' + senderName + '" src="' + senderLogo + '"></div><div class="dialog-message-body">' + messageText + '</div></div></div>';
+    var dialog = '<div class="dialog"><div class="dialogs-photo"><img alt="' + recipient.name + '" src="' + recipient.logo + '"></div><div class="dialogs-info"><div class="dialogs-info-name h3">' + recipient.name + '</div><div class="dialogs-info-date text-muted">' + messageTime + '</div></div><div class="dialogs-msg-contents"><div class="dialogs-message-image"><img alt="' + messageSender.name + '" src="' + messageSender.logo + '"></div><div class="dialog-message-body">' + messageText + '</div></div></div>';
     $('.wrapper').append(dialog);
     // If message hasn`t been read -> dark it 
-    if (!dialogs[indexDialog].read_status) {$('.wrapper .dialog:last-child').addClass('notread');}; 
+    if (!thisMessage.read_status) {$('.wrapper .dialog:last-child').addClass('notread');}; 
   }
 
   function lastDialogWithUser(recipientId) {
@@ -74,21 +65,19 @@ function  showDialogs() {
 
     if (recipientId == idThisUser) {
       // correspondence with userself
-      correspondence = _.filter(dialogs, function(element){ return (element.sender == idThisUser)&&(element.receiver == idThisUser); });
+      correspondence = _.filter(messages, function(element){ return (element.sender_id == idThisUser)&&(element.receiver_id == idThisUser); });
     } 
     else {
       // correspondence with another user
-      correspondence = _.filter(dialogs, function(element) {
-        return (element.sender == recipientId) || (element.receiver == recipientId);
+      correspondence = _.filter(messages, function(element) {
+        return (element.sender_id == recipientId) || (element.receiver_id == recipientId);
         });    
-    };
+    }; 
     // sort by time
-    correspondence = _.sortBy(correspondence, function(element){ return element.time; });
+    correspondence = _.sortBy(correspondence, function(element){ return element.created_at; });
     // add the lastest dialog with this user to list
     usersDialogList.push(_.last(correspondence));
   }
-
-
   // last dialogs
   var usersDialogList = [];
   // get last dialog with each user
@@ -96,34 +85,18 @@ function  showDialogs() {
     lastDialogWithUser(element.id);
   });
   // sort by time
-  usersDialogList = _.sortBy(usersDialogList, function(element){ return element.time; }).reverse();
+  usersDialogList = _.sortBy(usersDialogList, function(element){ return element.created_at; }).reverse();
   // last dialog with each user
   _.each(usersDialogList, function(element){
     addDialog(element.id);
   });  
 }
 
-
-
-
-  // this user's ID in users[]
-  var idThisUser = 1;
-  // var recipientId = 2;
-// console.log(usersDialogList); 
-
- // paramert - dialog ID
-// 
-// addDialog(3);
-// addDialog(4);
-// addDialog(5);
-// addDialog(6); // paramert - dialog ID
-// addDialog(7);
-// addDialog(8);
-// addDialog(9);
-// addDialog(10);
-
-// Show the list of users in the right column
+// this user's ID in users[]
+var idThisUser = 1;
+// Show dialogs in the left column
 showDialogs();
+// Show the list of users in the right column
 showUsers();
 // Show create-new-message form after click on user in the right column
 $('.right-column ul li').click(createNewMessage);
--- js/messages.js
+++ js/messages.js
@@ -1,42 +1,92 @@
 var messages = [
   {
     id: 1,
-    text: "Lorem ipsum dolor sit amet."
+    sender_id: 1, // message sender_id ID in users[]
+    receiver_id: 1, // message receiver_id ID in users[]
+    created_at: new Date(2015, 2, 1, 13, 8, 7),
+    text: "Lorem ipsum dolor sit amet.",
+    read_status: true, // true - message has been read
+    delete_status: false // true - message has been deleted
   },
   {
     id: 2,
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum sapiente excepturi esse inventore rerum tenetur, alias vitae quas mollitia repudiandae."
+    sender_id: 1,
+    receiver_id: 2,
+    created_at: new Date(2015, 2, 2, 14, 1, 15),
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum sapiente excepturi esse inventore rerum tenetur, alias vitae quas mollitia repudiandae.",
+    read_status: true,
+    delete_status: false
   },
   {
     id: 3,
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet, quisquam aliquam deleniti enim neque illum qui laboriosam quasi quod minima ipsam exercitationem maiores nostrum totam nulla animi voluptatem commodi voluptates iusto! Eaque, delectus vitae vel ducimus minus blanditiis ex, architecto fugit repellat asperiores amet quas itaque odio debitis et."
+    sender_id: 3,
+    receiver_id: 1,
+    created_at: new Date(2015, 2, 3, 15, 15, 1),
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet, quisquam aliquam deleniti enim neque illum qui laboriosam quasi quod minima ipsam exercitationem maiores nostrum totam nulla animi voluptatem commodi voluptates iusto! Eaque, delectus vitae vel ducimus minus blanditiis ex, architecto fugit repellat asperiores amet quas itaque odio debitis et.",
+    read_status: true,
+    delete_status: false
   },
   {
     id: 4,
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, provident!"
+    sender_id: 1,
+    receiver_id: 4,
+    created_at: new Date(2015, 2, 4, 16, 10, 5),
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, provident!",
+    read_status: true,
+    delete_status: false
   },
   {
     id: 5,
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    sender_id: 5,
+    receiver_id: 1,
+    created_at: new Date(2015, 2, 5, 17, 1, 1),
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    read_status: true,
+    delete_status: false
   },
   {
     id: 6,
-    text: "Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, harum. ipsum dolor sit amet."
+    sender_id: 1,
+    receiver_id: 1,
+    created_at: new Date(2015, 2, 4, 13, 8, 17),
+    text: "Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, harum. ipsum dolor sit amet.",
+    read_status: false,
+    delete_status: false
   },
   {
     id: 7,
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum sapiente excepturi esse inventore rerum tenetur, alias vitae quas mollitia repudiandae. Lorem1"
+    sender_id: 2,
+    receiver_id: 1,
+    created_at: new Date(2015, 2, 3, 14, 1, 25),
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum sapiente excepturi esse inventore rerum tenetur, alias vitae quas mollitia repudiandae. Lorem1",
+    read_status: true,
+    delete_status: false
   },
   {
     id: 8,
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    sender_id: 1,
+    receiver_id: 3,
+    created_at: new Date(2015, 2, 2, 15, 15, 11),
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    read_status: true,
+    delete_status: false
   },
   {
     id: 9,
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos eligendi optio sit neque aut eveniet inventore. Sapiente error, ab in! ipsum dolor sit amet, consectetur adipisicing elit. In, provident!"
+    sender_id: 4,
+    receiver_id: 1,
+    created_at: new Date(2015, 2, 1, 16, 10, 15),
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos eligendi optio sit neque aut eveniet inventore. Sapiente error, ab in! ipsum dolor sit amet, consectetur adipisicing elit. In, provident!",
+    read_status: true,
+    delete_status: false
   },
   {
     id: 10,
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut ea eaque explicabo ab maiores enim qui, nesciunt aspernatur corporis ad! ipsum dolor sit amet, consectetur adipisicing elit."
+    sender_id: 1,
+    receiver_id: 5,
+    created_at: new Date(2015, 2, 5, 17, 1, 11),
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut ea eaque explicabo ab maiores enim qui, nesciunt aspernatur corporis ad! ipsum dolor sit amet, consectetur adipisicing elit.",
+    read_status: false,
+    delete_status: false
   }
 ];
